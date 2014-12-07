ddfa379d976eb9eb35188bf78421e09562301c7c
Kira <rika_ordoba@mail.ru>
2014-12-07 23:47:40 +0300

    19 video

---
+++ FirstWebPage/FirstWebPage/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using FirstWebPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var model = new ArticleModel();
+            return View(model);
+        }
+
+    }
+}
---
+++ FirstWebPage/FirstWebPage/Models/ArticleModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace FirstWebPage.Models
+{
+    public class ArticleModel
+    {
+
+        public ArticleModel()
+        {
+            Title = "Это заголовок";
+            Body = "<p>Это обычный текст поста</p>";
+            Date = DateTime.Now;
+            Likes = new Collection<LikeModel>();
+            Comments = new Collection<CommentItemModel>();
+          
+        }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual ICollection<LikeModel> Likes { get; set; } //виртуальная чтобы не выгружать это сразу,а только когда обратишься(не забивать память)
+        public virtual ICollection<CommentItemModel> Comments { get; set; }
+    }
+}
